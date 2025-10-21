897fa25ffcae7bdbbd051f702a49395fa1dda4e6
RaphaVBueno <rafavbueno@gmail.com>
2025-10-21 17:46:03 -0300

    estruturação dos dados

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ async def root():
     forbes = get_forbes()
     g1 = get_g1()
     cnn = get_cnn()
-    return {"games_radar": games_radar, "games_pot": games_pot, "forbes": forbes, "g1": g1, "cnn": cnn}
+    return [{"id": 1, "title": games_radar, "url": "https://www.gamesradar.com/", "source": "gamesradar"}, {"id": 2, "title": games_pot, "url": "https://www.gamespot.com/", "source": "gamespot"}, {"id": 3,  "title": forbes, "url": "https://forbes.com.br/", "source": "forbes"}, {"id": 4,  "title": g1, "url": "https://g1.globo.com/", "source": "g1"}, {"id": 5,   "title": cnn, "url": "https://www.cnnbrasil.com.br/", "source": "cnn"}]
     
 
